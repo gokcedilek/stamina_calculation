342be2cd035b2dbfba0ec8be12f2cd35e38cfc60
Sathish Gopalakrishnan <sathish.gopalakrishnan@gmail.com>
2018-10-30 05:18:29 -0700

    initial version

---
+++ src/main/java/exercise13/WreckBeach.java
@@ -0,0 +1,10 @@
+package exercise13;
+
+public class WreckBeach {
+
+    public static int maxStamina(int[] stair, int stamina) {
+        // TODO implement this method
+        return -1;
+    }
+
+}
